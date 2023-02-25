837c507aaa35f71d6c070b27dcde0e385e3f5ebf
nmktad <nmktadesse@gmail.com>
2023-02-25 23:11:48 +0300

    minor fix

--- src/pages/user/patient/[id].tsx
+++ src/pages/user/patient/[id].tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { MyChart } from "../../../components/Chart";
 import UserLayout from "../../../layout/root";
 import { api } from "../../../utils/api";
+import { type GetServerSideProps } from "next";
 
 const Patient = () => {
   const router = useRouter();
@@ -19,11 +20,27 @@ const Patient = () => {
   if (!sessionData) {
     return <>No session data</>;
   }
+
   return (
     <UserLayout sessionData={sessionData}>
-      {JSON.stringify(patientData)}
+      <h1>{patientData?.name}</h1>
+      <sub>{patientData?.id}</sub>
+      <p>Admiited to: {patientData?.date}</p>
+
+      <p>Current BPM reading: {BPM}</p>
     </UserLayout>
   );
 };
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+// const res = await fetch("http://192.168.43.229/data");
+// const { BPM, SpO2 } = (await res.json()) as { BPM: number; SpO2: number };
+// return {
+//   props: {
+//     BPM,
+//     SpO2,
+//   },
+// };
+// };
 
 export default Patient;
